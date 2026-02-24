38efcd5504b56736f8a047aca6e0d60c86fbb2cd
Deahtstroke <dvm3099@pm.me>
2026-02-23 20:04:55 -0800

    feat/added config command (doesnt do anything yet)

---
+++ cmd/config/cmd.go
@@ -0,0 +1,21 @@
+package config
+
+import (
+	"github.com/deahtstroke/protheon/commands"
+	"github.com/spf13/cobra"
+)
+
+func Init() {
+	commands.RegisterCommand(newConfigCommand)
+}
+
+func newConfigCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "config COMMAND",
+		Short: "Manage saved Protheon ETL configs",
+		Long:  "Manage saved Protheon ETL configurations that are saved by the user",
+		Args:  cobra.ExactArgs(0),
+	}
+
+	return cmd
+}
